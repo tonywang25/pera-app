aedbf0eba67666850c855d75c31a4150bd0eb41b
Tony Wang <itstonywang@gmail.com>
2025-06-19 09:16:53 -0500

    Initial Commit

---
+++ pera-app/pera_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pera_appApp.swift
+//  pera-app
+//
+//  Created by Tony Wang on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct pera_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
